aa55515871404500afef88c39decd4df46e8faa5
Edward Seo <edwardseo@Edwards-MacBook-Pro.local>
2017-10-14 15:43:23 +0900

    assignmend 2 sample added

--- Assignment_02/code/main.m
+++ Assignment_02/code/main.m
@@ -3,7 +3,9 @@ clear;
 close all;
 
 % reading image
-img = imread('ginger.png');
+% img = imread('ginger.png');
+img = imread('nupjuk.jpeg');
+img = imresize(permute(img,[2,1,3]),0.1);
 figure, subplot(2,2,1)
 imshow(img), title('original image')
 [row,col,~] = size(img);
